d1a5a1363e70784b6dae5ea33f470198cd9facb4
Racter <paracetamolboy@gmail.com>
2016-09-28 13:59:06 +0200

    Added support for the DRFDocs module to auto-generate HTTP API documentation and expose it on /docs.Fixed mixed tabs/spaces issue.

--- seed_service_rating/urls.py
+++ seed_service_rating/urls.py
@@ -9,7 +9,7 @@ admin.site.site_header = os.environ.get('SERVICE_RATING_TITLE',
 urlpatterns = patterns(
     '',
     url(r'^admin/',  include(admin.site.urls)),
-	url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^docs/', include('rest_framework_docs.urls')),
     url(r'^api/auth/',
         include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/token-auth/',
