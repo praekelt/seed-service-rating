51d5ec09ccebaf10a525b82dea4747b0fc9e4a1e
gsvr <george@westerncapelabs.com>
2016-09-30 13:30:29 +0200

    move logic to ndoh-hub

--- seed_service_rating/settings.py
+++ seed_service_rating/settings.py
@@ -184,14 +184,6 @@ TOTAL_INVITES_TO_SEND = int(os.environ.get('TOTAL_INVITES_TO_SEND', '2'))
 
 DAYS_BETWEEN_INVITES = int(os.environ.get('DAYS_BETWEEN_INVITES', '7'))
 
-# Will also parse a list, eg. 'momconnect_prebirth, momconnect_postbirth'
-INVITE_REG_TYPES = [reg_type.strip() for reg_type in os.environ.get(
-    'INVITE_REG_TYPES', 'momconnect_prebirth').split(',')]
-
-# Will also parse a list, eg. 'hw_full, hw_partial'
-INVITE_AUTH_LEVEL = [auth_level.strip() for auth_level in os.environ.get(
-    'INVITE_AUTH_LEVEL', 'hw_full').split(',')]
-
 INVITE_TEXT = os.environ.get(
     'INVITE_TEXT',
     'Thank you for registering. We can only improve if we get your feedback. '
